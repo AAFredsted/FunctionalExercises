e219738fa6a782ff4e8a30774628520a7c37b497
AAFredsted <Andra_Fredsted@proton.me>
2024-04-04 14:55:34 +0200

    added 8.2

--- Exercises8_aandr.fsx
+++ Exercises8_aandr.fsx
@@ -27,3 +27,27 @@ let rec countA acc tree =
 //as this function contains two nested recursive calls, it cannot be written as an iterative function in f#, 
 //thus not being tail-recursive
 
+
+//Exercise 8.2
+(*
+Declare a tail-recursive functions with the type:  countAC : BinTree<’a> -> int -> (int -> ’b) -> ’b, 
+such that count t = countAC t 0 id. The intuition with countAC t a c is that a is the
+number of nodes being counted so far and c is the continuation.
+*)
+
+//this solution combines pattern matching and a continuation to 
+let rec countAC tree acc cont =
+    if tree = Leaf then 
+        cont acc
+    else 
+        match tree with
+        | Node (Leaf, _, Leaf) ->  
+            cont (acc + 1)
+        | Node (t1, _, Leaf) ->    
+            countAC t1 (acc + 1) cont
+        | Node (Leaf, _, t2) ->   
+            countAC t2 (acc + 1) cont
+        | Node (t1, _, t2) ->    
+            countAC t1 acc (fun acc' -> countAC t2 acc' cont)
+//this is bad, but I have no other idea of what to do..
+
